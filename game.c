723085ba0ad8aa9a21364dc5b2bc428017ed38ab
Connor Milligan <connoramilligan@gmail.com>
2023-09-28 18:02:42 -0400

    rewrite of selectors

--- game.c
+++ game.c
@@ -14,17 +14,26 @@ bool quitPrompt = false;
 enum gameState currentState = TitleScreen;
 
 void gameLoop() {
+    uint8 c = 0, choice = 0;
+
     while (gameRunning) {
-        //gotoxy(3,5);
-        //cprintf("%d", c);
-        draw();
-        takeInput();
+        draw(&c, &choice);
+
+        c = 12;
+        gotoxy(3,5);
+        cprintf("value of c: %d", c);
+        gotoxy(3,6);
+        cprintf("value of choice: %d", choice);
+
+        takeInput(&c, &choice);
     }
     
 }
 
-void draw() {
-    clrscr();
+void draw(uint8 *c, uint8 *choice) {
+    if (!quitPrompt && currentState != NamePrompt) {
+        clrscr();
+    }
 
     if (currentState == TitleScreen) {
         menuDrawTitleScreen();
@@ -32,25 +41,42 @@ void draw() {
     else if (currentState == NamePrompt) {
         menuDrawNamePrompt();
     }
+
+    if (quitPrompt) {
+        menuDrawQuitPrompt(*choice);
+    }
 }
 
-void takeInput() {
-    uint8 c = 0;
-        
+void takeInput(uint8 *c, uint8 *choice) {
+    *c = cgetc();
+
     if (quitPrompt) {
-        menuDrawQuitPrompt();
-        gameRunning = selectorQuitPrompt() ? true : false;
-        quitPrompt = false;
-    } else {
-        c = cgetc();
+        if (*c == KEY_DOWN || *c == KEY_UP) {
+            *choice = *choice ? 0 : 1;
+        } 
+        else if (*c == KEY_RETURN) {
+            if (*choice == 0) {
+                gameRunning = false;
+            }
+            else {
+                quitPrompt = false;
+            }
+        }
     }
 
     // Title Screen prompts
-    if (c == KEY_RETURN && currentState == TitleScreen) {
+    if (*c == KEY_RETURN && currentState == TitleScreen && !quitPrompt) {
         currentState = NamePrompt;
+        clrscr();
+    }
+
+    else if (currentState == NamePrompt) {
+        uint8 x = (XSize)/2 - 12, y = YSize/2+1;
+
+        
     }
         
-    if (c == 3) {
+    if (*c == 3) {
         quitPrompt = true;
     }
 }
